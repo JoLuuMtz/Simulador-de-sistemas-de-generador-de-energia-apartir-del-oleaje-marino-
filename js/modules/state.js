01b6c2e89e41c0d5ec3a6e5d63c0b8f4973ffcb2
JoLu Martinez <jorgemartinezricardo@gmail.com>
2025-09-25 13:23:32 -0500

    Empezando las modificiones    del control del oelaje con las persianas

--- js/modules/state.js
+++ js/modules/state.js
@@ -6,15 +6,15 @@ canvas.width = window.innerWidth;
 canvas.height = 400;
 
 export const state = {
-  time: 0,
-  energy: 0,
-  maxEnergy: 100,
-  waveHeight: 30,
-  waveLength: 100,
-  waveSpeed: 0.05,
-  offset: 0,
-  isWaveActive: false,
-  floater: { x: canvas.width / 2, y: canvas.height / 2, width: 40, height: 20, prevY: 0 },
-  coil: { x: canvas.width / 2, y: canvas.height / 2 + 30, width: 10, height: 30 },
-  battery: { x: canvas.width - 140, y: 340, width: 100, height: 30 },
+    time: 0,
+    energy: 0,
+    maxEnergy: 100,
+    waveHeight: 30,
+    waveLength: 100,
+    waveSpeed: 0.05,
+    offset: 0,
+    isWaveActive: false,
+    floater: { x: canvas.width / 2, y: canvas.height / 2, width: 40, height: 20, prevY: 0 },
+    coil: { x: canvas.width / 2, y: canvas.height / 2 + 30, width: 10, height: 30 },
+    battery: { x: canvas.width - 140, y: 340, width: 100, height: 30 },
 };
